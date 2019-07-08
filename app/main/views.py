dc6c2188d226c464be87ef96423fcd1835fd11d8
Soniawangui <koisonia99@gmail.com>
2019-07-08 08:59:24 +0300

    Setup SMTP server and configure the port to use the gmail SMTP server port

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
-from .. import db
+from .. import db,photos
 from flask_login import login_required
 from ..models import User,Blog
 from .forms import UpdateProfile
@@ -45,3 +45,14 @@ def index():
 def new_blog():    
    blogform = BlogForm()
 
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
